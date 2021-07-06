c1efabcef7dc76a1471bf65a82f53cbc7852f240
Michiah <Michiah.Grainger@gmail.com>
2021-07-06 14:50:05 -0400

    lab

---
+++ src/sample/Controller.java
@@ -0,0 +1,18 @@
+package sample;
+
+import java.sql.SQLOutput;
+
+public class Controller {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Name: Michiah Grainger");
+        System.out.println("Hometown: West Palm Beach");
+        System.out.println("Major: Computer Science");
+        System.out.println("Favorite Song: Started From The Bottom by Drake");
+        System.out.println("Favorite Superhero: Spiderman");
+        System.out.println("Favorite Caffeine Source: Coffee");
+    }
+
+
+
+}
